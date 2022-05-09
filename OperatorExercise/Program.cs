8b07cad20518afe796bb365c96430d00915a624d
sstoveri <sstoveri45@gmail.com>
2022-05-09 10:53:53 -0500

    "Completed Exercise 1"

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -7,13 +7,24 @@ namespace OperatorExercise
         static void Main(string[] args)
         {
             //-----------------------------------------------Exercise 1------------------------------------------------
+            double a = 17;
+            double b = 4;
+            double quotient = a / b;
+            double remainder = a % b;
 
+            Console.WriteLine($"{a} added to {b} equals to {a + b}");
+            Console.WriteLine($"{a} subtracted by {b} equals to {a - b}");
+            Console.WriteLine($"{a} muliplied by {b} equals to {a * b}");
+            Console.WriteLine($"{a} divided by {b} equals to {a / b}");
 
-
-            int a = 17;
-            int b = 4;
-            int quotient = a / b;
-            int remainder = a % b;
+            double answer = Add(a, b);
+            Console.WriteLine(answer);
+            double answer1 = Subtract(a, b);
+            Console.WriteLine(answer1);
+            double answer2 = Multiply(a, b);
+            Console.WriteLine(answer2);
+            double answer3 = Divide(a, b);
+            Console.WriteLine(answer3);
 
 
             if (a == 17 && b == 4)
@@ -27,6 +38,22 @@ namespace OperatorExercise
 
 
 
+        }
+        public static double Add( double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            return a / b;
         }
 
         public static double AreaofCicle(double r)
