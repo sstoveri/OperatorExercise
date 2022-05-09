631ba773ed0d23d43dd1dd068e928e1320572739
sstoveri <sstoveri45@gmail.com>
2022-05-09 10:34:37 -0500

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------Exercise 1------------------------------------------------
+
+
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            }
+
+            //-----------------------------------------------Exercise 2------------------------------------------------
+
+
+
+
+
+        }
+
+        public static double AreaofCicle(double r)
+        {
+            return Math.PI * Math.Max(r, 2);
+        }
+    }
+}
